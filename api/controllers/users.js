0cb6f548724d57aa7ecd43f5b70d21957b2b9270
Jaguso <jagutzo@gmail.com>
2019-05-11 19:11:52 -0500

    made signup with another syntax

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,11 +1,31 @@
 const { Users } = require('../models');
 
-exports.user_signup = async(req, res, next) => {
-    let user = await Users.create(req.body);
-    if (!user) res.status(400).json({ message: 'Could not create user' });
+exports.user_signup = (req, res, next) => {
+    // let user = await Users.create(req.body);
+    // if (!user) res.status(400).json({ message: 'Could not create user' });
 
-    return res.status(201).json({
-        message: "User created",
-        id: user.id
-    })
+    // return res.status(201).json({
+    //     message: "User created",
+    //     id: user.id
+    // })
+
+    const user = new Users({
+        username: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    user.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'User created',
+                id: user.id
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 }
--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const userRoutes = require('./api/routes/users');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/user', userRoutes);
+app.use('/users', userRoutes);
 
 
 module.exports = app;
